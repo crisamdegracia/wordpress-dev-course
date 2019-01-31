b0d80bd98ffdc3c07a079b5f9e6fa75606466bc5
Crisam De Gracia <crisamdegracia@gmail.com>
2019-01-31 03:45:32 +0300

    folder 8 -- Events custom post type, advanced custom field -- first commit

--- wordpress/wp-content/themes/Exodus/front-page.php
+++ wordpress/wp-content/themes/Exodus/front-page.php
@@ -18,29 +18,51 @@
     <div class="full-width-split__inner">
       <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-      <?php 
-  $homepageEvents = new WP_Query(array(
-      "posts_per_page"     => 2,
-      "post_type"         => 'event'
-  ));    
+      <?php
+  $today = date("Ymd");
 
-    while( $homepageEvents->have_posts() ) :
+$homepageEvents = new WP_Query(array(
+  "posts_per_page"       => 2,
+  "post_type"            => 'event',
+  "meta_key"             => 'event_date',
+  "orderby"             => 'meta_value_num',
+  "order"                => 'ASC',
+  "meta_query"           => array(
+    array(
+      'key' => 'event_date',
+      'compare' => '>=',
+      'value'   => $today,
+      'type'    => 'numeric'
+    )
+  )
 
-    $homepageEvents->the_post(); ?>
-    
+));    
+
+while( $homepageEvents->have_posts() ) :
+
+$homepageEvents->the_post(); ?>
+
       <div class="event-summary">
         <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
-          <span class="event-summary__month"> <?php the_time('M') ?></span>
-          <span class="event-summary__day"><?php the_time('d') ?></span>  
+          <span class="event-summary__month">
+            <?php $eventDate = new DateTime( get_field('event_date'));
+           echo  $eventDate->format('M'); ?>
+          </span>
+          <span class="event-summary__day"><?php  echo  $eventDate->format('d'); ?></span>  
         </a>
         <div class="event-summary__content">
           <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
-          <p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
+          <p><?php if ( has_excerpt() ) {
+
+  echo get_the_excerpt();
+} else { 
+  echo wp_trim_words(get_the_content(), 18 );
+} ?><a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
         </div>
       </div>
-        
+
       <?php endwhile;?>
-      <p class="t-center no-margin"><a href="<?php echo site_url('/events') ?>" class="btn btn--blue">View All Events</a></p>
+      <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" class="btn btn--blue">View All Events</a></p>
 
     </div>
   </div>
@@ -49,10 +71,10 @@
       <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
 
       <?php $homePagePosts = new WP_Query(array(
-        'posts_per_page' => 2
-      ));
+  'posts_per_page' => 2
+));
 
-while( $homePagePosts->have_posts() ) : $homePagePosts->the_post();   ?>
+                                       while( $homePagePosts->have_posts() ) : $homePagePosts->the_post();   ?>
 
       <div class="event-summary">
         <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink() ?>">
@@ -61,7 +83,12 @@ while( $homePagePosts->have_posts() ) : $homePagePosts->the_post();   ?>
         </a>
         <div class="event-summary__content">
           <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
-          <p><?php echo wp_trim_words(get_the_content(), 18 ); ?> <a href="<?php the_permalink() ?>" class="nu gray">Read more</a></p>
+          <p><?php if ( has_excerpt() ) {
+
+  echo get_the_excerpt();
+} else { 
+  echo wp_trim_words(get_the_content(), 18 );
+} ?> <a href="<?php the_permalink() ?>" class="nu gray">Read more</a></p>
         </div>
       </div>
 
