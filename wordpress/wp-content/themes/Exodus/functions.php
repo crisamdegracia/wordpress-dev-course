46d0b4bea5cd78957de84e222ae3fc695c20af76
Crisam De Gracia <crisamdegracia@gmail.com>
2019-03-06 01:28:56 +0300

    done folder 14: WordPress REST API with LIVE SEARCH JS

--- wordpress/wp-content/themes/Exodus/functions.php
+++ wordpress/wp-content/themes/Exodus/functions.php
@@ -30,7 +30,6 @@ function pageBanner($args = NULL) {
 <?php }
 
 function wp_theme_styles(){
-
   wp_enqueue_script( 'googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyD-rsOXjG5-vXQEjd-YFC4zBBEEAb8tl6w', NULL, microtime() , true); 
 
   wp_enqueue_script( 'main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime() , true);
@@ -39,6 +38,13 @@ function wp_theme_styles(){
   wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style( 'main-style', get_stylesheet_uri(), NULL, microtime() );
+
+
+  
+  wp_enqueue_script('main-university-js', get_template_directory_uri() . '/test.js');
+  wp_localize_script('main-university-js', 'universityData',
+                     array( 'root_url' => get_site_url() ) );
+
 }
 
 add_action('wp_enqueue_scripts', 'wp_theme_styles');
--- wordpress/wp-content/themes/Exodus/js/modules/Search.js
+++ wordpress/wp-content/themes/Exodus/js/modules/Search.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
 
+
 class Search {
   //describe and create/initiate our project
   constructor() {
+    this.addSearchHTML();
     this.openButton = $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
@@ -12,11 +14,10 @@ class Search {
     this.isOverlayOpen = false;
     this.typingTimer;
     this.isSpinnerVisible = false;
-    this.previousValue;
+    this.previousValue; 
   }
 
   //events
-
   events() {
     this.openButton.on("click", this.openOverlay.bind(this));
     this.closeButton.on("click", this.closeOverlay.bind(this));
@@ -39,7 +40,7 @@ class Search {
           this.resultsDiv.html('<div class="spinner-loader"></div>');
           this.isSpinnerVisible =  true;
         }
-        this.typingTimer = setTimeout( this.getResults.bind(this) , 2000);
+        this.typingTimer = setTimeout( this.getResults.bind(this) , 500);
 
       } else {
         this.resultsDiv.html("");
@@ -49,22 +50,53 @@ class Search {
 
     this.previousValue = this.searchField.val();
   }
-  getResults() {
-  
-    $.getJSON('http://localhost/wp-json/wp/v2/posts?search='+ this.searchField.val(), posts => {
-      this.resultsDiv.html(`
-    <h2 class="search-overlay__section-title"> General Information </h2>
-    <ul class="link-list min-list">
-      <li><a href="${posts[0].link}"> ${posts[0].title.rendered} </a> </li>
-    </ul>
 
-`);
-    })
+
+
+  getResults() {
+    // Asynchronous style JS -- 
+    //on  when() we can use many JSON request as we want
+    //and it will all run asynchronous 
+    $.when(
+      // 1st argument -- can have many argument 
+      //normaly we pass two  argment LOCATION and FUNCTION on JSON but
+      // on when() method its okay to be one. coz we use then()
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search='+ this.searchField.val()),
+
+      // 2nd argument  
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search='+ this.searchField.val())
+
+      //when all JSON request are complete it will run
+      //a anomous function using ES6 
+      // every request from when will match the pagkasunod sunod ng
+      // parameter ex.  when(a,b,c) is then(a,b,c)
+      //using posts and pages we can output request from JSON
+    ).then((posts, pages) => {
+      // we combined 2 array by using concat()
+      // posts[0] - is becoz we only use the many array information
+      // giving the when() and then() method
+      // we only use the first array of information
+      //the first item is the first JSON data
+      //2nd is whether it fail or succeded
+      var combinedResults = posts[0].concat(pages[0]);
+
+      //outputing on the HTML
+      this.resultsDiv.html(`
+<h2 class="search-overlay__section-title"> General Information </h2>
+${combinedResults.length ? '<ul class="link-list min-list">' : '<p> No results found </p>'}
+${combinedResults.map( item => `<li><a href="${item.link}">  ${item.title.rendered} </a></li>` ).join('')}
+${combinedResults.length ? '</ul>' : '' }
+
+`)
+    }, () => {
+      this.resultsDiv.html('<p> Unexpected Error. Please try again! </p>')
+    });
+    this.isSpinnerVisible = false;
   }
-  
-  
+
+
   keyPressDispatcher(e){
-    if(e.keyCode == 83 && ! this.isOverlayOpen && !$("input, textarea").is("focus")){
+    if(e.keyCode == 83 && ! this.isOverlayOpen && !$("input, textarea").is("focus") ){
 
       this.openOverlay();
       console.log('open overlay');
@@ -79,6 +111,8 @@ class Search {
   openOverlay(){
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val('');
+    setTimeout(() => this.searchField.focus(), 301  );
     this.isOverlayOpen = true;
   }
 
@@ -87,6 +121,35 @@ class Search {
     $("body").removeClass("body-no-scroll");
     this.isOverlayOpen = false;
   }
+
+
+  addSearchHTML(){
+
+    $("body").append(`
+
+
+<div class="search-overlay">
+<div class="search-overlay__top">
+<div class="container">
+<i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+<input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+<i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+</div>
+</div>
+
+<div class="container">
+<div id="search-overlay__results">
+
+</div>
+</div>
+</div>
+
+`)
+
+
+  }
+
+
 }
 
 export default Search;
